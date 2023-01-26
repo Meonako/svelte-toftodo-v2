3e7af550ee67ff2e5b3681a7b11abc62420da960
Meonako <76484203+Meonako@users.noreply.github.com>
2023-01-26 20:57:17 +0700

    Update: Convert `string` to `Date` object

--- src/lib/utils/getDefault.ts
+++ src/lib/utils/getDefault.ts
@@ -1,11 +1,19 @@
 import { browser } from "$app/environment";
+import type { Base } from "./interface";
 
-export function getDefault<T>(key: string, def: T): T {
+export function getDefault<T extends Base>(key: string, def: T): T {
 	if (browser) {
 		const ObjectString = window.localStorage.getItem(key);
 		if (!ObjectString) return def;
 
-		return JSON.parse(ObjectString);
+		let Object = JSON.parse(ObjectString);
+		if (!Object.Time) return def;
+
+		if (typeof Object.Time == 'string') {
+			Object.Time = new Date(Object.Time);
+		}
+
+		return Object
 	}
 	return def;
 }
