37d1f0cf9ad43746a8da02c15a14073384f8ce5f
Meonako <76484203+Meonako@users.noreply.github.com>
2023-02-26 09:59:15 +0700

    Fix: reset.ts

--- src/lib/utils/reset.ts
+++ src/lib/utils/reset.ts
@@ -1,4 +1,4 @@
-import type { Base } from "$lib/store/interface";
+import type { Base } from "$lib/utils/interface";
 
 export function reset<Type extends Base>(data: Type): Type {
 	for (const [key, value] of Object.entries(data)) {
@@ -6,14 +6,14 @@ export function reset<Type extends Base>(data: Type): Type {
 
 		if (typeof value == 'number') {
 			// @ts-ignore
-			data[key as keyof Monthly] = 0;
+			data[key as keyof Type] = 0;
 		} else if (typeof value == 'boolean') {
 			// @ts-ignore
-			data[key as keyof Monthly] = false;
+			data[key as keyof Type] = false;
 		} else if (typeof value == 'object') {
 			for (const key2 of Object.keys(value)) {
 				// @ts-ignore
-				data[key as keyof Monthly][key2 as keyof Object] = false;
+				data[key as keyof Type][key2 as keyof Object] = false;
 			}
 		} else {
 			console.log('Not found type: ', value, typeof value);
