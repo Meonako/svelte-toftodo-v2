cc7abd31a41ba96a0af27b10c644e905b2e58a23
Meonako <76484203+Meonako@users.noreply.github.com>
2023-02-26 09:40:56 +0700

    Add: Bring back side scroll with mouse wheel
    Add: Bring back side scroll with mouse wheel
    Update: Styling a little bit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,36 @@
 	import Daily from '$lib/section/Daily.svelte';
 	import Weekly from '$lib/section/Weekly.svelte';
 	import Monthly from '$lib/section/Monthly.svelte';
+
+	function transformScroll(event: WheelEvent) {
+		if (!event.deltaY) return;
+		if (!event.currentTarget) return;
+		const target = event.currentTarget as HTMLElement;
+		sideScroll(target, event.deltaY > 0 ? 'right' : 'left', 10, event.deltaY, 7);
+	}
+	function sideScroll(
+		element: HTMLElement,
+		direction: 'left' | 'right',
+		speed: number,
+		distance: number,
+		step: number
+	) {
+		let scrollAmount = 0;
+		let slideTimer = setInterval(() => {
+			if (direction == 'left') {
+				element.scrollLeft -= step;
+			} else {
+				element.scrollLeft += step;
+			}
+			scrollAmount += step;
+			if (scrollAmount >= Math.abs(distance)) {
+				clearInterval(slideTimer);
+			}
+		}, speed);
+	}
 </script>
 
-<main>
+<main on:wheel={transformScroll}>
 	<div class="item"><Daily/></div>
 	<div class="item"><Weekly/></div>
 	<div class="item"><Monthly/></div>
@@ -13,8 +40,9 @@
 <style lang="scss">
 	main {
 		text-align: center;
-		padding: 3rem 5rem 4rem 5rem;
-		// border: 1px solid white;
+		padding: 0 2.5rem .3rem 2.5rem;
+		margin: 1rem 2.5rem 0 2.5rem;
+		border: 1px solid white;
 		backdrop-filter: blur(5px);
 		white-space: nowrap;
 		overflow-x: scroll;
